8f8956976258c8df6a4643903e0f4eb078b9708f
tunacim <tunaz783@gmail.com>
2023-03-02 20:29:20 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {View,Text,SafeAreaView} from "react-native"
+function App(){
+  return(
+    <SafeAreaView>
+      <View>
+        <Text></Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App;
